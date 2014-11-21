a612272461a003811be8e51d1d09f7e3571e145d
Chandramouli Soorian <cs@quantads.net>
2014-11-21 19:24:22 +0100

    New source.

---
+++ src/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+
+import Data.Csv
+
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+main :: IO ()
+main = do
+  csvData <- BL.readFile "battling.csv"
+  let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
+  let summed = fmap (V.foldr summer 0) v
+  putStrLn $ "Total atBats was: " ++ (show summed)
+  where summer (name, year, team, atBats) n = n + atBats
